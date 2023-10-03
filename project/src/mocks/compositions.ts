2523ad5a659c4cb1e4312d22845e9f31a7ef8191
Roman Katrovsky <katrovsky@gmail.com>
2023-10-03 22:37:02 +0300

    добавлены моки с одной композицией

---
+++ project/src/mocks/compositions.ts
@@ -0,0 +1,14 @@
+import { Composition } from '../types/comosition';
+
+export const compositions: Composition[] = [
+  {
+    id: 1,
+    rating: 2,
+    author: 'Оззи Осборн',
+    name: 'Goodbye To Romance',
+    genre: 'Рок',
+    record: 'records/GoodbyeToRomance.webm',
+    poster: 'img/compositions/ozzy-osbourne.webp',
+    data: '27-08-2023',
+  }
+];
---
+++ project/src/types/comosition.ts
@@ -0,0 +1,12 @@
+export type Composition = {
+  id: number;
+  rating: number;
+  author: string;
+  name: string;
+  genre: string;
+  record: string;
+  poster: string;
+  data: string;
+}
+
+export type Compositions = Composition[];
