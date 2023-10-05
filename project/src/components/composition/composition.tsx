aee8df3f8fe4e6e382b6666b793babe8ff5b42c4
Roman Katrovsky <katrovsky@gmail.com>
2023-10-05 23:34:27 +0300

    Вносит в моки все композиции

--- project/src/components/composition/composition.tsx
+++ project/src/components/composition/composition.tsx
@@ -8,11 +8,11 @@ function Composition({composition}: CompositionProps): JSX.Element {
   return (
     <section className="records-list__composition composition">
       <h3 className="composition-band main-middle-title">{composition.author}</h3>
-      <p className="composition-title">Goodbye To Romance</p>
-      <video className="composition-video" controls preload="none" poster="img/compositions/ozzy-osbourne.webp">
-        <source src="records/GoodbyeToRomance.webm" type="video/webm" />
+      <p className="composition-title">{composition.name}</p>
+      <video className="composition-video" controls preload="none" poster={composition.poster}>
+        <source src={composition.record} type="video/webm" />
       </video>
-      <p className="composition-date visually-hidden">27 августа 2023</p>
+      <p className="composition-date visually-hidden">{composition.data}</p>
     </section>
   );
 }
--- project/src/mocks/compositions.ts
+++ project/src/mocks/compositions.ts
@@ -9,7 +9,93 @@ export const compositions: CompositionType[] = [
     genre: 'Рок',
     record: 'records/GoodbyeToRomance.webm',
     poster: 'img/compositions/ozzy-osbourne.webp',
-    data: '27-08-2023',
+    data: '27 августа 2023',
     duration: '05:30'
+  },
+
+  {
+    id: 2,
+    rating: 3,
+    author: 'Джо Завинул',
+    name: 'Mercy Mercy Mercy',
+    genre: 'Джаз',
+    record: 'records/MercyMercyMercy.webm',
+    poster: 'img/compositions/joe-zawinul.webp',
+    data: '08 июля 2023',
+    duration: '03:41'
+  },
+
+  {
+    id: 3,
+    rating: 4,
+    author: 'Перси Мэйфилд',
+    name: 'Hit the Road Jack',
+    genre: 'Джаз',
+    record: 'records/HitTheRoadJack.webm',
+    poster: 'img/compositions/ray-charles.webp',
+    data: '08 июля 2023',
+    duration: '02:03'
+  },
+
+  {
+    id: 4,
+    rating: 1,
+    author: 'Квин',
+    name: 'Bohemian Rhapsody',
+    genre: 'Рок',
+    record: 'records/BohemianRhapsody.webm',
+    poster: 'img/compositions/queen.webp',
+    data: '19 августа 2022',
+    duration: '05:04'
+  },
+
+  {
+    id: 5,
+    rating: 1,
+    author: 'Лало Шифрин',
+    name: 'Mission Impossible',
+    genre: 'Джаз',
+    record: 'records/MissionImpossible.webm',
+    poster: 'img/compositions/lalo-schifrin.webp',
+    data: '16 сентября 2022',
+    duration: '02:31'
+  },
+
+  {
+    id: 6,
+    rating: 1,
+    author: 'Пинк Флойд',
+    name: 'Shine On You Crazy Diamond (Part V)',
+    genre: 'Рок',
+    record: 'records/ShineOnYouCrazyDiamond.webm',
+    poster: 'img/compositions/pink-floyd.webp',
+    data: '11 ноября 2022',
+    duration: '02:45'
+  },
+
+  {
+    id: 7,
+    rating: 1,
+    author: 'Европа',
+    name: 'The Final Countdown',
+    genre: 'Рок',
+    record: 'records/TheFinalCountdown.webm',
+    poster: 'img/compositions/europe.webp',
+    data: '11 октября 2022',
+    duration: '04:04'
+  },
+
+  {
+    id: 8,
+    rating: 1,
+    author: 'Скорпионс',
+    name: 'Still Loving You',
+    genre: 'Рок',
+    record: 'records/StillLovingYou.webm',
+    poster: 'img/compositions/scorpions.webp',
+    data: '24 сентября 2022',
+    duration: '04:42'
   }
+
+
 ];
