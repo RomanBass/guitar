acec9ff57edb94021e4fb6257ce48ab56ebd8207
Roman Katrovsky Huawei Laptop <katrovsky@gmail.com>
2023-10-28 00:03:10 +0300

    wip поправил рабочую директорию для линтера и делает меню фильтров через map

--- project/src/components/filter/filter.tsx
+++ project/src/components/filter/filter.tsx
@@ -1,20 +1,15 @@
+const filterOptions = ['Все', 'Рок', 'Джаз', 'Классика'];
+
+
 function Filter(): JSX.Element {
-  return (
-    <ul className="records__filter filter">
-      <li className="filter__filter-option filter-option">
-        <a href="/" className="filter-option_filter-text filter-text filter-text--active">Все</a>
-      </li>
-      <li className="filter__filter-option filter-option">
-        <a href="/" className="filter-option_filter-text filter-text">Рок</a>
-      </li>
-      <li className="filter__filter-option filter-option">
-        <a href="/" className="filter-option_filter-text filter-text">Джаз</a>
-      </li>
-      <li className="filter__filter-option filter-option">
-        <a href="/" className="filter-option_filter-text filter-text">Классика</a>
-      </li>
-    </ul>
-  );
+
+  const filterList = filterOptions.map((filterOption) => (
+    <li value={filterOption} key={filterOption} className="filter__filter-option filter-option">
+      <a href="/" className={`filter-option_filter-text filter-text ${filterOption === 'Рок' ? 'filter-text--active' : ''}`}>{filterOption}</a>
+    </li>
+  ));
+
+  return <ul className="records__filter filter">{filterList}</ul>;
 }
 
 export default Filter;
